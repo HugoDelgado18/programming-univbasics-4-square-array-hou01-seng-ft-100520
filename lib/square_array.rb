69a093f8df1cb6cdcbf8813cb0254f1a53db02f4
Hugo Delgado <hugo.delgado1@outlook.com>
2020-08-19 00:32:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,8 @@ def square_array(array)
   counter = 0
 
   while array[counter] do
-    return new_array << array[counter] ** 2
+    new_array << array[counter] ** 2
     counter += 1
+    return new_array
   end
 end
